9a0080a021b2a313a85b3e0d9668d08f8c042da4
Amean Asad <amean.asad1999@gmail.com>
2020-11-22 20:17:19 -0500

    Regression

--- dataPreprocessing.py
+++ dataPreprocessing.py
@@ -8,6 +8,11 @@ Created on Sat Nov 21 20:40:13 2020
 
 
 import pandas as pd
+import numpy as np
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+
 
 dataLocation = "EmployeeData"
 
@@ -35,21 +40,25 @@ generalData = generalData.drop(columns=["EmployeeID"])
 
 categoricalData =  list(generalData.select_dtypes(include='object').columns)
 
+# 1 -> Employee left
+# 0 -> Employee remains
+turnover = pd.DataFrame(np.where(turnover == "Yes", 1, 0), columns=["Turnover"])
 
 
 def encodeStringCategories(columns, df):
     """
     Parameters
     ----------
-    columns : TYPE
-        DESCRIPTION.
-    df : TYPE
-        DESCRIPTION.
+    columns : List
+        List of string columns to encode.
+    df : Pandas Dataframe
+        Dataframe containing the feature data.
 
     Returns
     -------
-    df : TYPE
-        DESCRIPTION.
+    df : Pandas Dataframe
+        Dataframe containing feature data with all the categorical data being
+        one-hot encoded.
     """
     for column in columns:
         oneHotEncoding = pd.get_dummies(df[column])
@@ -59,15 +68,43 @@ def encodeStringCategories(columns, df):
 
 
 data = encodeStringCategories(categoricalData, generalData)
+data = data.join(turnover)
+
+# Remove any null valued data
 data = data.fillna(0);
-data = ((data-data.min())/(data.max()-data.min()))
+
+# Data Normalization
+data = ((data-data.min())/(data.max() - data.min()))
 
 
+train, test = train_test_split(data, test_size=0.25)
+
+trainY = train["Turnover"]
+trainX = train.drop(columns=["Turnover"])
+
+testY = test["Turnover"]
+testY = testY.to_numpy(dtype=int);
+testX = test.drop(columns=["Turnover"])
 
 
+regressor = LogisticRegression()
 
 
+regressor.fit(trainX, trainY)
 
+predictions = regressor.predict(testX)
+
+
+count = 0
+for i in range(len(predictions)):
+    if (predictions[i] == testY[i]):
+        count+=1
+    
+
+print(count/len(testY))
+        
+    
+    
 
 
 
