dfb8d4a66801c6086d493a363d9874be28ef87ec
Amean Asad <amean.asad1999@gmail.com>
2020-12-12 23:29:32 -0500

    contributer chart

--- dataPreprocessing.py
+++ dataPreprocessing.py
@@ -119,19 +119,18 @@ coefficients = coefficients.sort_values(by=["abs_weights"], ascending=False)
 negativeCoefficients = coefficients[coefficients["weights"] < 0]
 positiveCoefficients = coefficients[coefficients["weights"] > 0]
 
-coefficients = coefficients.head(15)
-
+topCoefficients = coefficients.head(15)
 
 
 cm = metrics.confusion_matrix(testY, predictions)
 
-chart = px.bar(x=coefficients["features"],
-               y=coefficients["weights"], orientation="v")
+chart = px.bar(x=topCoefficients["features"],
+               y=topCoefficients["weights"], orientation="v")
 chart.update_layout(transition={
-                'duration': 500,
-                'easing': 'cubic-in-out'
-                })
-    
+    'duration': 500,
+    'easing': 'cubic-in-out'
+})
+
 # print(items.head)
 #classes = list(testX.columns)
 #print(len(coefficients), len(classes));
@@ -144,15 +143,40 @@ chart.update_layout(transition={
 #######################################################################
 #######################################################################
 
+
 def getLabels(df):
     labels = list(df.columns)
     labels = [{"value": x, "label": x} for x in labels]
     return labels
 
-#####################################################################
+
+def getCoefficients(df, model):
+    weights = pd.DataFrame(np.transpose(model.coef_))
+
+    coefficients = pd.concat([pd.DataFrame(testX.columns),
+                              weights,
+                              abs(weights)], axis=1)
+
+    coefficients.columns = ["features", "weights", "abs_weights"]
+    coefficients = coefficients.sort_values(
+        by=["abs_weights"], ascending=False)
+    coefficients = coefficients.head(15)
+
+    return coefficients
+
+
+def getPositiveCoefficients(coefficients):
+    negativeCoefficients = coefficients[coefficients["weights"] > 0]
+    return negativeCoefficients.head(10)
+
+
+def getNegativeCoefficients(coefficients):
+    positiveCoefficients = coefficients[coefficients["weights"] < 0]
+    return positiveCoefficients.head(10)
+
 
 #####################################################################
-
+#####################################################################
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 
@@ -172,13 +196,34 @@ app.layout = html.Div(children=[
     dbc.Row(
         [
             dbc.Col(dcc.Graph(id='top-labels',
-                              figure=chart),),
+                              figure=chart),sm=6),
             dbc.Col([
                 dbc.Row(dropDown()),
                 dbc.Row(dcc.Graph(id='histograms'))
-                    ]),
+            ], sm=6),
         ]
     ),
+    dbc.Row(
+        [
+            dbc.Col(dcc.Graph(id='top-contributers',
+                              figure=chart),sm=6),
+            dbc.Col([
+                dbc.Row(dcc.RadioItems(
+                        id='radio-items',
+                        options=[
+                            {'label': '   Positive   ', 'value': 'positive'},
+                            {'label': '   Negative   ', 'value': 'negative'},
+                        ],
+                        value="positive",
+                        labelStyle= {"margin-right":"2%"},
+                        style={"width":"100%"}
+                        ), 
+                        style={"width": "100%"}),
+                dbc.Row(dcc.Graph(id='contributers'))
+            ], sm=6),
+
+        ]
+    )
 ],   style={
     'padding': "2%"
 })
@@ -186,16 +231,33 @@ app.layout = html.Div(children=[
 
 @app.callback(
     [Output('histograms', 'figure')],
-     Input('time-series-dropdown', 'value')
+    Input('time-series-dropdown', 'value')
 )
 def chart(value):
     vals = generalData[value].to_numpy()
     chart = px.histogram(x=vals)
     chart.update_layout(transition_duration=2000,
-                             title="Number of Daily Issues")
+                        title="Number of Daily Issues")
 
     return [chart]
 
+
+@app.callback(
+    [Output('contributers', 'figure')],
+    Input('radio-items', 'value')
+)
+def chart(value):
+    if value == "positive":
+        df = getPositiveCoefficients(coefficients)
+    else:
+        df = getNegativeCoefficients(coefficients)
+
+    chart = px.pie(df, values='abs_weights', names='features',
+                   title='Population of European continent')
+    chart.update_layout(transition_duration=2000,
+                        title="Number of Daily Issues")
+
+    return [chart]
 
 
 if __name__ == '__main__':
