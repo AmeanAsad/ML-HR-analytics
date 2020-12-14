4a9c7dafb3cbdf7849382a09cc755d189bdf3984
Amean Asad <amean.asad1999@gmail.com>
2020-12-13 19:55:57 -0500

    model modifications

--- dataPreprocessing.py
+++ dataPreprocessing.py
@@ -27,6 +27,7 @@ from sklearn.model_selection import train_test_split
 
 dataLocation = "EmployeeData"
 
+
 # Prepare file directories for each data surveyed
 generalDataLocation = dataLocation + "/general_data.csv"
 employeeSurveyLocation = dataLocation + "/employee_survey_data.csv"
@@ -36,8 +37,11 @@ generalData = pd.read_csv(generalDataLocation)
 generalData.sort_values(by=['EmployeeID'])
 turnover = generalData["Attrition"]
 generalData = generalData.drop(
-    columns=["Attrition", "Over18", "EmployeeCount", "StandardHours"])
+    columns=["Over18", "EmployeeCount", "StandardHours"])
 
+generalData.to_json(path_or_buf = "ProcessedData/data.json", orient="records" )
+
+generalData = generalData.drop(columns=["Attrition"])
 managerSurveyData = pd.read_csv(managerSurveyLocation)
 employeeSurveyData = pd.read_csv(employeeSurveyLocation)
 
@@ -88,6 +92,7 @@ data = data.fillna(0)
 # Data Normalization
 data = ((data-data.min())/(data.max() - data.min()))
 
+data.to_json(path_or_buf="ProcessedData/normalizedData.json")
 
 train, test = train_test_split(data, test_size=0.25)
 
@@ -112,153 +117,11 @@ coefficients = pd.concat([pd.DataFrame(testX.columns),
                           weights,
                           abs(weights)], axis=1)
 
-
 coefficients.columns = ["features", "weights", "abs_weights"]
 coefficients = coefficients.sort_values(by=["abs_weights"], ascending=False)
-
-negativeCoefficients = coefficients[coefficients["weights"] < 0]
-positiveCoefficients = coefficients[coefficients["weights"] > 0]
+coefficients.to_json(path_or_buf="ProcessedData/coefficients.json")
 
 topCoefficients = coefficients.head(15)
 
-
 cm = metrics.confusion_matrix(testY, predictions)
 
-chart = px.bar(x=topCoefficients["features"],
-               y=topCoefficients["weights"], orientation="v")
-chart.update_layout(transition={
-    'duration': 500,
-    'easing': 'cubic-in-out'
-})
-
-# print(items.head)
-#classes = list(testX.columns)
-#print(len(coefficients), len(classes));
-
-# plt.figure()
-#X = coefficients["feautres"].to_numpy()[10]
-#Y = coefficients["weights"].to_numpy()[10]
-# plt.show()
-
-#######################################################################
-#######################################################################
-
-
-def getLabels(df):
-    labels = list(df.columns)
-    labels = [{"value": x, "label": x} for x in labels]
-    return labels
-
-
-def getCoefficients(df, model):
-    weights = pd.DataFrame(np.transpose(model.coef_))
-
-    coefficients = pd.concat([pd.DataFrame(testX.columns),
-                              weights,
-                              abs(weights)], axis=1)
-
-    coefficients.columns = ["features", "weights", "abs_weights"]
-    coefficients = coefficients.sort_values(
-        by=["abs_weights"], ascending=False)
-    coefficients = coefficients.head(15)
-
-    return coefficients
-
-
-def getPositiveCoefficients(coefficients):
-    negativeCoefficients = coefficients[coefficients["weights"] > 0]
-    return negativeCoefficients.head(10)
-
-
-def getNegativeCoefficients(coefficients):
-    positiveCoefficients = coefficients[coefficients["weights"] < 0]
-    return positiveCoefficients.head(10)
-
-
-#####################################################################
-#####################################################################
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
-
-
-def dropDown():
-    dropdown = dcc.Dropdown(
-        id='time-series-dropdown',
-        options=getLabels(generalData),
-        value="Age",
-        style={"width": "100%"}
-    )
-    return dropdown
-
-
-app.layout = html.Div(children=[
-    dbc.Row(html.H1(children='Git Analysis with Python'), justify='center'),
-    html.Div(id='data', style={'display': 'none'}),
-    dbc.Row(
-        [
-            dbc.Col(dcc.Graph(id='top-labels',
-                              figure=chart),sm=6),
-            dbc.Col([
-                dbc.Row(dropDown()),
-                dbc.Row(dcc.Graph(id='histograms'))
-            ], sm=6),
-        ]
-    ),
-    dbc.Row(
-        [
-            dbc.Col(dcc.Graph(id='top-contributers',
-                              figure=chart),sm=6),
-            dbc.Col([
-                dbc.Row(dcc.RadioItems(
-                        id='radio-items',
-                        options=[
-                            {'label': '   Positive   ', 'value': 'positive'},
-                            {'label': '   Negative   ', 'value': 'negative'},
-                        ],
-                        value="positive",
-                        labelStyle= {"margin-right":"2%"},
-                        style={"width":"100%"}
-                        ), 
-                        style={"width": "100%"}),
-                dbc.Row(dcc.Graph(id='contributers'))
-            ], sm=6),
-
-        ]
-    )
-],   style={
-    'padding': "2%"
-})
-
-
-@app.callback(
-    [Output('histograms', 'figure')],
-    Input('time-series-dropdown', 'value')
-)
-def chart(value):
-    vals = generalData[value].to_numpy()
-    chart = px.histogram(x=vals)
-    chart.update_layout(transition_duration=2000,
-                        title="Number of Daily Issues")
-
-    return [chart]
-
-
-@app.callback(
-    [Output('contributers', 'figure')],
-    Input('radio-items', 'value')
-)
-def chart(value):
-    if value == "positive":
-        df = getPositiveCoefficients(coefficients)
-    else:
-        df = getNegativeCoefficients(coefficients)
-
-    chart = px.pie(df, values='abs_weights', names='features',
-                   title='Population of European continent')
-    chart.update_layout(transition_duration=2000,
-                        title="Number of Daily Issues")
-
-    return [chart]
-
-
-if __name__ == '__main__':
-    app.run_server(debug=True, use_reloader=False)
---
+++ utils.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 13 19:54:51 2020
+
+@author: amean
+"""
+
